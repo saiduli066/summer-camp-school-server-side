c5200082ed81d2d4c199a2c17df51e312d597089
Md Saidul Islam <saiduli066@gmail.comm>
2023-06-11 01:12:08 +0600

    popular teachers functionalities are added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const classes = require("./classes.json");
+const teachers = require("./teachers.json");
 
 // middleware
 app.use(cors());
@@ -19,6 +20,10 @@ app.get("/classes", (req, res) => {
   res.send(classes);
 });
 
+app.get('/teachers', (req, res) => {
+    res.send(teachers);
+})
+
 
 
 // popular classes...
@@ -31,6 +36,19 @@ app.get("/popularClasses", (req, res) => {
     const popularClasses = sortedClasses.slice(0, 6);
     console.log(popularClasses);
   res.send(popularClasses);
+});
+
+
+// popular teachers...
+
+app.get("/popularTeachers", (req, res) => {
+
+    const sortedTeachers = teachers.teachers.sort(
+      (a, b) => b.enrolledStudents - a.enrolledStudents
+    );
+    const popularTeachers = sortedTeachers.slice(0, 6);
+    console.log(popularTeachers);
+  res.send(popularTeachers);
 });
 
 app.listen(port, () => {
