2141ca5479166245a6de8e8882c4cbf053eea997
Md Saidul Islam <saiduli066@gmail.comm>
2023-06-14 22:54:45 +0600

    vercel resolved

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
